f82d765ebc176b9853c9ac0b8b5d4ae0cfb3cf40
bmoby <msouleymanov@gmail.com>
2016-09-20 21:04:39 +0200

    bmoby day 1

--- iOsBmoby/AppDelegate.swift
+++ iOsBmoby/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Parse
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,6 +17,18 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        
+        //Configuration of using Parse in iOsBmoby
+        let parseConfig = ParseClientConfiguration { (ParseMutableClientConfiguration) in
+            
+            //Accessing heroku App vio ID & Keys
+            ParseMutableClientConfiguration.applicationId = "bmoby8789"
+            ParseMutableClientConfiguration.clientKey = "bmoby1107198730091989"
+            ParseMutableClientConfiguration.server = "http://bmoby.herokuapp.com/parse"
+        }
+        
+        Parse.initializeWithConfiguration(parseConfig)
+        
         return true
     }
 
---
+++ iOsBmoby/listing.swift
@@ -0,0 +1,16 @@
+//
+//  listing.swift
+//  iOsBmoby
+//
+//  Created by Magomed Souleymnov on 20/09/2016.
+//  Copyright © 2016 BMoby. All rights reserved.
+//
+
+import Foundation
+
+class listing{
+    var longitude :String = "0"
+    var latitude :String = "0"
+    var streetNb :String = "0"
+    var streetName :String = "0"
+}
